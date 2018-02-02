0174a216be5e0b9d4aba0ea27976bb21e9d0a7d3
Bert Green <bertg@fwl117.fz-rossendorf.de>
2018-02-02 13:11:40 +0100

    Committer: Bert Green <bertg@fwl117.fz-rossendorf.de>
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   src/FastDAQ.cpp
    	new file:   src/FastDAQ.h
    	new file:   src/config.log
    	new file:   src/config.status

---
+++ src/FastDAQ.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : FastDAQ.cpp
+// Author      : Bertram Green
+// Version     :
+// Copyright   : Property of Helmholtz Association
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Fast DAQ Beginning" << endl; // prints Fast DAQ Beginning
+
+	// Initialize ADC
+
+	// set up trigger
+	// set up buffer
+	// Set up channels
+
+	// Initialize capture card
+
+	// set up camera
+
+
+	// maybe stream data to file?
+	// continuous scan mode?
+
+
+
+	return 0;
+}
---
+++ src/FastDAQ.h
@@ -0,0 +1,16 @@
+/*
+ * FastDAQ.h
+ *
+ *  Created on: Feb 2, 2018
+ *      Author: bertg
+ */
+
+#ifndef FASTDAQ_H_
+#define FASTDAQ_H_
+
+
+
+
+
+
+#endif /* FASTDAQ_H_ */
